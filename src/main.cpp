1f72c30cd8dc66216fdf3dba9dac652190c48153
Rugtveit <BenjaminRugtveit@gmail.com>
2020-10-26 16:06:28 +0100

    Removed codegen from code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include  "include/main.hpp"
-#include "codegen/include/UnityEngine/Random.hpp"
 #include <algorithm>
+#include <cmath>
 
 
 ModConfig::Config_t ModConfig::Config;
@@ -12,28 +12,34 @@ const Logger& getLogger() {
   static const Logger& logger(modInfo);
   return logger;
 }
+
 /// @brief Return a random hsv color in UnityEngine Color (RGBA)
-UnityEngine::Color getRandomHSV()
+Color getRandomHSV()
 {
-    float h = UnityEngine::Mathf::Lerp(0.0f, 1.0f, UnityEngine::Random::get_value());
-    float s = UnityEngine::Mathf::Lerp(1.0f, 1.0f, UnityEngine::Random::get_value());
-    float v = UnityEngine::Mathf::Lerp(1.0f, 1.0f, UnityEngine::Random::get_value());
-    UnityEngine::Color result = UnityEngine::Color::HSVToRGB(h, s, v, true);
-    result.a = UnityEngine::Mathf::Lerp(1.0f, 1.0f, UnityEngine::Random::get_value());
+    RET_DEFAULT_UNLESS(il2cpp_utils::RunMethod("UnityEngine", "Random", "InitState", 582));
+    float randomFloat1 = RET_DEFAULT_UNLESS(il2cpp_utils::RunMethod<float>("UnityEngine", "Random", "get_value"));
+    //float randomFloat2 = RET_DEFAULT_UNLESS(il2cpp_utils::RunMethod<float>("UnityEngine", "Random", "get_value"));
+    //float randomFloat3 = RET_DEFAULT_UNLESS(il2cpp_utils::RunMethod<float>("UnityEngine", "Random", "get_value"));
+    //float randomFloat4 = RET_DEFAULT_UNLESS(il2cpp_utils::RunMethod<float>("UnityEngine", "Random", "get_value"));
+    float h = std::lerp(0.0f, 1.0f, randomFloat1);
+    float s = std::lerp(1.0f, 1.0f, randomFloat1);
+    float v = std::lerp(1.0f, 1.0f, randomFloat1);
+    Color result = RET_DEFAULT_UNLESS(il2cpp_utils::RunMethod<Color>("UnityEngine", "Color", "HSVToRGB", h, s, v, true));
+    result.a = std::lerp(1.0f, 1.0f, randomFloat1);
     return result;
 }
 /// @brief Converts Color to Color32
 /// @param color UnityEngine Color input you want to convert
-UnityEngine::Color32 ColorToColor32(UnityEngine::Color color)
+Il2CppObject* ColorToColor32(Color color)
 {
-    uint8_t r = UnityEngine::Mathf::Max(0, UnityEngine::Mathf::Min(255, (int)UnityEngine::Mathf::FloorToInt( color.r* 256.0f)));
-    uint8_t g = UnityEngine::Mathf::Max(0, UnityEngine::Mathf::Min(255, (int)UnityEngine::Mathf::FloorToInt( color.g* 256.0f)));
-    uint8_t b = UnityEngine::Mathf::Max(0, UnityEngine::Mathf::Min(255, (int)UnityEngine::Mathf::FloorToInt( color.b* 256.0f)));
-    uint8_t a = UnityEngine::Mathf::Max(0, UnityEngine::Mathf::Min(255, (int)UnityEngine::Mathf::FloorToInt( color.a* 256.0f)));
-    return UnityEngine::Color32(r,g,b,a);
+    uint8_t r = std::max(0, std::min(255, (int)std::floor(color.r*256.0f)));
+    uint8_t g = std::max(0, std::min(255, (int)std::floor(color.g*256.0f)));
+    uint8_t b = std::max(0, std::min(255, (int)std::floor(color.b*256.0f)));
+    uint8_t a = std::max(0, std::min(255, (int)std::floor(color.a*256.0f)));
+    return RET_DEFAULT_UNLESS(il2cpp_utils::New("UnityEngine", "Color32", r, g, b, a));
 }
 
-MAKE_HOOK_OFFSETLESS(StretchableObstacle_SetSizeAndColor, void, GlobalNamespace::StretchableObstacle* self, float width, float height, float length, UnityEngine::Color color)
+MAKE_HOOK_OFFSETLESS(StretchableObstacle_SetSizeAndColor, void, Il2CppObject* self, float width, float height, float length, Color color)
 {
     // Enabling transparent walls
     // I get the obstacleCore from self (instance)
@@ -42,12 +48,9 @@ MAKE_HOOK_OFFSETLESS(StretchableObstacle_SetSizeAndColor, void, GlobalNamespace:
     // Also this is seemed as cheating on PC so this is to make it fair between platforms.
     if(ModConfig::Config.transparentWalls)
     {
-        auto* obstacleCore = self->obstacleCore; 
-        getLogger().info("obstacleCore: %p", obstacleCore);
-
-        auto obstacleCoreGO = obstacleCore->get_gameObject();
-        getLogger().info("obstacleCoreGO: %p", obstacleCoreGO);
-        obstacleCoreGO->SetActive(false);
+        Il2CppObject* obstacleCore = RET_V_UNLESS(il2cpp_utils::GetFieldValue(self, "_obstacleCore"));
+        Il2CppObject* obstacleCoreGO = RET_V_UNLESS(il2cpp_utils::GetPropertyValue(obstacleCore, "gameObject"));
+        RET_V_UNLESS(il2cpp_utils::RunMethod(obstacleCoreGO, "SetActive", false));
 
     }
     // Disabling fake glow on walls
@@ -55,13 +58,11 @@ MAKE_HOOK_OFFSETLESS(StretchableObstacle_SetSizeAndColor, void, GlobalNamespace:
     // I get the gameObject of obstacle fake glow and make it inactive.
     if(!ModConfig::Config.fakeGlowOnWalls)
     {
-        auto obstacleFakeGlow = self->obstacleFakeGlow;
-        getLogger().info("obstacleFakeGlow: %p", obstacleFakeGlow);
-
-        auto obstacleFakeGlowGO = obstacleFakeGlow->get_gameObject();
+        Il2CppObject* _obstacleFakeGlow  = RET_V_UNLESS(il2cpp_utils::GetFieldValue(self, "_obstacleCore"));
+        auto obstacleFakeGlowGO = RET_V_UNLESS(il2cpp_utils::GetPropertyValue(_obstacleFakeGlow, "gameObject"));
         getLogger().info("obstacleFakeGlowGO: %p", obstacleFakeGlowGO);
 
-        obstacleFakeGlowGO->SetActive(false);
+        RET_V_UNLESS(il2cpp_utils::RunMethod(obstacleFakeGlowGO, "SetActive", false));
     }
 
     getLogger().info("Done setting fakeGlow to %d and ModConfig::Config.transparentWalls to %d", ModConfig::Config.fakeGlowOnWalls, ModConfig::Config.transparentWalls);
@@ -77,19 +78,27 @@ MAKE_HOOK_OFFSETLESS(ObstacleSaberSparkleEffectManager_Update, void, Il2CppObjec
     if(ModConfig::Config.obstacleSaberSparkle) ObstacleSaberSparkleEffectManager_Update(self);
 }
 
-MAKE_HOOK_OFFSETLESS(NoteCutParticlesEffect_SpawnParticles, void, GlobalNamespace::NoteCutParticlesEffect* self, Vector3 pos, Vector3 cutNormal, Vector3 saberDir, 
-                    Vector3 moveVec, UnityEngine::Color32 color, int sparkleParticlesCount, int explosionParticlesCount, float lifeTimeMultiplier, int saberType)
+MAKE_HOOK_OFFSETLESS(NoteCutParticlesEffect_SpawnParticles, void, Il2CppObject* self, Vector3 pos, Vector3 cutNormal, Vector3 saberDir, 
+                    Vector3 moveVec, Il2CppObject* color, int sparkleParticlesCount, int explosionParticlesCount, float lifeTimeMultiplier)
 {
     // Particles
     // Multiply default count with config multipliers
     // Floor the final value since the method expects ints
-    sparkleParticlesCount = UnityEngine::Mathf::FloorToInt(sparkleParticlesCount * ModConfig::Config.slashParticleMultiplier); 
-    explosionParticlesCount = UnityEngine::Mathf::FloorToInt(sparkleParticlesCount * ModConfig::Config.explosionParticleMultiplier); 
+    sparkleParticlesCount = (int)std::floor(sparkleParticlesCount * ModConfig::Config.slashParticleMultiplier); 
+    explosionParticlesCount = (int)std::floor(sparkleParticlesCount * ModConfig::Config.explosionParticleMultiplier); 
+
+    Il2CppObject* sparkleModule = RET_V_UNLESS(il2cpp_utils::GetFieldValue(self, "_sparklesPSMainModule"));
+    RET_V_UNLESS(il2cpp_utils::SetPropertyValue(sparkleModule, "maxParticles", INT_MAX));
+
+    Il2CppObject* sparkleModule = RET_V_UNLESS(il2cpp_utils::GetFieldValue(self, "_sparklesPSMainModule"));
+    RET_V_UNLESS(il2cpp_utils::SetPropertyValue(sparkleModule, "maxParticles", INT_MAX));
+
+
     // Rainbow particles
     // Get a random hsv color and convert it from a color to color32
     // Method expects a color32 and not color
     if(ModConfig::Config.rainbowParticles) color = ColorToColor32(getRandomHSV());
-    NoteCutParticlesEffect_SpawnParticles(self, pos, cutNormal, saberDir, moveVec, color, sparkleParticlesCount, explosionParticlesCount, lifeTimeMultiplier, saberType);
+    NoteCutParticlesEffect_SpawnParticles(self, pos, cutNormal, saberDir, moveVec, color, sparkleParticlesCount, explosionParticlesCount, lifeTimeMultiplier);
     // Score submission
     // Disabling scoresubmission if particle count is over the treshold of lagging the game.
     // This lag can give users a big advantage since the game can accidently calculate the cut of a note twice
@@ -103,26 +112,25 @@ MAKE_HOOK_OFFSETLESS(NoteCutParticlesEffect_SpawnParticles, void, GlobalNamespac
 
 }
 
-UnityEngine::Color noGlowColor = {0.0f, 0.0f, 0.0f, 0.0f};
-MAKE_HOOK_OFFSETLESS(SetSaberFakeGlowColor_SetColors, void, GlobalNamespace::SetSaberFakeGlowColor* self)
+Color noGlowColor = {0.0f, 0.0f, 0.0f, 0.0f};
+MAKE_HOOK_OFFSETLESS(SetSaberFakeGlowColor_SetColors, void, Il2CppObject* self)
 {
     // Disabling fake glow on sabers
     // Run base game method if fakeGlowOnSabers is true
     // else change parametric3SliceSprite.color to be 0 on all color channels
     // Refresh the sprites so the color gets updated for the sprites. 
     if(ModConfig::Config.fakeGlowOnSabers) return SetSaberFakeGlowColor_SetColors(self);
-    auto* parametric3SliceSprite = self->parametric3SliceSprite;
-    parametric3SliceSprite->color = noGlowColor;
-    parametric3SliceSprite->Refresh();
+    Il2CppObject* parametric3SliceSprite = RET_V_UNLESS(il2cpp_utils::GetFieldValue(self, "_parametric3SliceSprite"));
+    RET_V_UNLESS(il2cpp_utils::SetFieldValue(parametric3SliceSprite, "color", noGlowColor));
+    RET_V_UNLESS(il2cpp_utils::RunMethod(parametric3SliceSprite, "Refresh"));
 }
 
-MAKE_HOOK_OFFSETLESS(SaberClashChecker_Update, void, Il2CppObject* self)
+MAKE_HOOK_OFFSETLESS(SaberClashEffect_LateUpdate, void, Il2CppObject* self)
 {
     // Disabling SaberClash
     // Run base game method if saberClash is true
     // or else do nothing
-    if(ModConfig::Config.saberClash) return SaberClashChecker_Update(self);
-    //CRASH_UNLESS(il2cpp_utils::SetPropertyValue(self,"sabersAreClashing", false));
+    if(ModConfig::Config.saberClash) return SaberClashEffect_LateUpdate(self);
 }
 
 extern "C" void setup(ModInfo& info) 
@@ -142,7 +150,7 @@ extern "C" void load()
     if (!ModConfig::loadConfig()) ModConfig::saveConfig();
     INSTALL_HOOK_OFFSETLESS(StretchableObstacle_SetSizeAndColor, il2cpp_utils::FindMethodUnsafe("", "StretchableObstacle", "SetSizeAndColor", 4));
     INSTALL_HOOK_OFFSETLESS(ObstacleSaberSparkleEffectManager_Update, il2cpp_utils::FindMethodUnsafe("", "ObstacleSaberSparkleEffectManager", "Update", 0));
-    INSTALL_HOOK_OFFSETLESS(NoteCutParticlesEffect_SpawnParticles, il2cpp_utils::FindMethodUnsafe("", "NoteCutParticlesEffect", "SpawnParticles", 9));
+    INSTALL_HOOK_OFFSETLESS(NoteCutParticlesEffect_SpawnParticles, il2cpp_utils::FindMethodUnsafe("", "NoteCutParticlesEffect", "SpawnParticles", 8));
     INSTALL_HOOK_OFFSETLESS(SetSaberFakeGlowColor_SetColors, il2cpp_utils::FindMethodUnsafe("", "SetSaberFakeGlowColor", "SetColors", 0));
-    INSTALL_HOOK_OFFSETLESS(SaberClashChecker_Update, il2cpp_utils::FindMethodUnsafe("", "SaberClashChecker", "Update", 0));
+    INSTALL_HOOK_OFFSETLESS(SaberClashEffect_LateUpdate, il2cpp_utils::FindMethodUnsafe("", "SaberClashEffect", "LateUpdate", 0));
 }
